253a64dff814d470f2701d36db37d7f479082bb9
Sree Harsha C S <sreeharshacs@gmail.com>
2024-12-12 15:41:36 -0800

    Using com_github_mvukov_rules_ros2 for ros integration

--- ros/test/rosbag_reader_test.cpp
+++ ros/test/rosbag_reader_test.cpp
@@ -5,14 +5,24 @@
 #include <rosbag2_cpp/writers/sequential_writer.hpp>
 #include <rosbag2_storage/serialized_bag_message.hpp>
 #include <filesystem>
+#include <cstdlib>  // For getenv and setenv
 
+#include <nav_msgs/msg/odometry.hpp>
 class RosbagReaderTest : public ::testing::Test {
 protected:
     void SetUp() override {
-        rclcpp::init(0, nullptr);
+        // Set the ROS logging directory to the test temporary directory
+        const char* test_tmpdir = std::getenv("TEST_TMPDIR");
+        if (test_tmpdir) {
+            setenv("ROS_LOG_DIR", test_tmpdir, 1);
+            setenv("ROS_HOME", test_tmpdir, 1);
+        }
+
+        rclcpp::InitOptions init_options;
+        rclcpp::init(0, nullptr, init_options);
+
         graph_ = std::make_unique<core::graph::FactorGraph>();
         store_ = std::make_unique<core::storage::MapStore>();
-        createTestBag();
         config_.odom_topic = "/odom";
         config_.color_topic = "/camera/color/image_raw";
         config_.depth_topic = "/camera/depth/image_rect_raw";
@@ -20,55 +30,10 @@ protected:
     }
 
     void TearDown() override {
-        if (std::filesystem::exists(test_bag_path_)) {
-            std::filesystem::remove(test_bag_path_);
-        }
         rclcpp::shutdown();
     }
 
-    void createTestBag() {
-        rosbag2_cpp::Writer writer;
-        writer.open(test_bag_path_);
-
-        rclcpp::Time time(0);
-        for (int i = 0; i < 10; ++i) {
-            time += rclcpp::Duration::from_seconds(0.1);
-
-            // Add odometry
-            auto odom_msg = std::make_shared<nav_msgs::msg::Odometry>();
-            odom_msg->header.stamp = time;
-            odom_msg->header.frame_id = "odom";
-            odom_msg->child_frame_id = "base_link";
-            odom_msg->pose.pose.position.x = i * 0.1;
-            odom_msg->pose.pose.orientation.w = 1.0;
-            writer.write(odom_msg, config_.odom_topic, time);
-
-            // Add images and camera info
-            auto color_msg = std::make_shared<sensor_msgs::msg::Image>();
-            color_msg->header.stamp = time;
-            color_msg->height = 480;
-            color_msg->width = 640;
-            color_msg->encoding = "rgb8";
-            color_msg->data.resize(640 * 480 * 3, i);
-            writer.write(color_msg, config_.color_topic, time);
-
-            auto depth_msg = std::make_shared<sensor_msgs::msg::Image>();
-            depth_msg->header.stamp = time;
-            depth_msg->height = 480;
-            depth_msg->width = 640;
-            depth_msg->encoding = "16UC1";
-            depth_msg->data.resize(640 * 480 * 2, i);
-            writer.write(depth_msg, config_.depth_topic, time);
-
-            auto camera_info = std::make_shared<sensor_msgs::msg::CameraInfo>();
-            camera_info->height = 480;
-            camera_info->width = 640;
-            camera_info->k = {500.0, 0.0, 320.0, 0.0, 500.0, 240.0, 0.0, 0.0, 1.0};
-            writer.write(camera_info, config_.camera_info_topic, time);
-        }
-    }
-
-    std::string test_bag_path_ = "/tmp/test_ros2.db3";
+    std::string test_bag_path_ = "/mnt/remote-storage/all_topics_2024_12_05_19_23_52";
     ros::Config config_;
     std::unique_ptr<core::graph::FactorGraph> graph_;
     std::unique_ptr<core::storage::MapStore> store_;
